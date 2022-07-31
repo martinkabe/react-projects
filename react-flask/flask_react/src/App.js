67acd2d40b4d6c8b7a588d31ded96c80cc53fad6
Martin <martinkabe@seznam.cz>
2022-07-31 11:57:36 +0200

    react part

---
+++ react-flask/flask_react/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+
+function App() {
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    fetch('/members').then(res => res.json()).then(data => {
+      setData(data);
+      // console.log(data)
+    });
+  }, []);
+
+  return (
+    <div id='cust-div'>
+
+      {(typeof data.members === 'undefined') ? (
+        <p>Loading ...</p>
+      ): (
+        data.members.map((member, i) => (
+          <p key={i}>{member} is active now ...</p>
+        ))
+      )}
+
+    </div>
+  );
+}
+
+export default App;
